5517670675f4fd72e043353d52140dbcb65ba3c3
Mareike <mareike.brandt@hotmail.de>
2022-03-21 18:22:58 +0900

    basic structure done

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react/cjs/react.development";
+import "./App.css";
+import { Form } from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>to-do:</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
